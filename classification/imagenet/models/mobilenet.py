8827dbd20174b0df7ff171e826441d7f82ebf669
wangpeisong <peisong.wang@nlpr.ia.ac.cn>
2019-03-03 18:23:16 +0800

    add mobilenet

---
+++ classification/imagenet/models/mobilenet.py
@@ -0,0 +1,72 @@
+import torch.nn as nn
+import torch.utils.model_zoo as model_zoo
+
+__all__ = ['mobilenet']
+
+
+model_urls = {
+    'mobilenet': '/home/wangpeisong/data/cnn_models/pytorch/mobilenet/mobilenet-00000000.pth',
+}
+
+
+def conv_bn(inp, oup, stride):
+    return nn.Sequential(
+        nn.Conv2d(inp, oup, 3, stride, 1, bias=False),
+        nn.BatchNorm2d(oup),
+        nn.ReLU(inplace=True)
+    )
+
+
+def conv_dw(inp, oup, stride):
+    return nn.Sequential(
+        nn.Conv2d(inp, inp, 3, stride, 1, groups=inp, bias=False),
+        nn.BatchNorm2d(inp),
+        nn.ReLU(inplace=True),
+
+        nn.Conv2d(inp, oup, 1, 1, 0, bias=False),
+        nn.BatchNorm2d(oup),
+        nn.ReLU(inplace=True),
+    )
+
+
+class MobileNet(nn.Module):
+    def __init__(self, num_classes=1000):
+        super(MobileNet, self).__init__()
+        self.features = nn.Sequential(
+            conv_bn(  3,  32, 2), 
+            conv_dw( 32,  64, 1),
+            conv_dw( 64, 128, 2),
+            conv_dw(128, 128, 1),
+            conv_dw(128, 256, 2),
+            conv_dw(256, 256, 1),
+            conv_dw(256, 512, 2),
+            conv_dw(512, 512, 1),
+            conv_dw(512, 512, 1),
+            conv_dw(512, 512, 1),
+            conv_dw(512, 512, 1),
+            conv_dw(512, 512, 1),
+            conv_dw(512, 1024, 2),
+            conv_dw(1024, 1024, 1),
+            nn.AvgPool2d(7),
+        )
+        self.classifier = nn.Linear(1024, 1000)
+
+    def forward(self, x):
+        x = self.features(x)
+        x = x.view(-1, 1024)
+        x = self.classifier(x)
+        return x
+
+
+def mobilenet(pretrained=False, **kwargs):
+    r"""Mobilenet model architecture from the
+    `"One weird trick..." <https://arxiv.org/abs/1404.5997>`_ paper.
+
+    Args:
+        pretrained (bool): If True, returns a model pre-trained on ImageNet
+    """
+    model = MobileNet(**kwargs)
+    if pretrained:
+        model.load_state_dict(model_zoo.load_url(model_urls['mobilenet']))
+    return model
+
